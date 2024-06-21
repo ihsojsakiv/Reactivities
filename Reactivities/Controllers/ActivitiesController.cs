782685208c636d4fd81a7172836e189fbbd7b52a
ihsojsakiv <vikasjoshi02@gmail.com>
2024-06-21 21:21:30 +0530

    Activity Changes

---
+++ Reactivities/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+using Domain;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Reactivities.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivity()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
